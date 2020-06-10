557a4cd7855dd380bdb41830258fecee9a87e810
Klaudia <k.polan95@gmail.com>
2020-06-10 02:03:04 +0200

    add file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FlightsModule } from "./flights/flights.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
