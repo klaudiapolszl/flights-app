be1e963200c2413d2f2b754acc7a7b9edf72b44d
Klaudia <k.polan95@gmail.com>
2020-06-10 02:01:38 +0200

    add file

--- src/app/core/services/flights.service.ts
+++ src/app/core/services/flights.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Component } from '@angular/core';
 import { Observable } from "rxjs/Observable";
-import { HttpClient } from "@angular/common/http";
-import { HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Flights } from "../../models/flight.model";
 import 'rxjs/Rx';
 import * as _ from 'lodash';
@@ -60,24 +59,45 @@ export class FlightsService {
         console.log("The POST observable is now completed.");
       });
   }
-
-  deleteFlight(key): Boolean {
-    let status = false;
+  async deleteFlight(key: string) {
     this.http.delete(this.API_URL+"/flights/"+key+".json")
-      .subscribe(
-          (val) => {
-              console.log("DELETE call successful value returned in body",
-                          val);
-              status = true;
-          },
-          response => {
-              console.log("DELETE call in error", response);
-              status = false;
-          },
-          () => {
-              console.log("The DELETE observable is now completed.");
-              status = true;
-          });
-    return status;
+    .subscribe(
+      (val) => {
+          console.log("DELETE call successful value returned in body",
+                      val);
+      },
+      response => {
+          console.log("DELETE call in error", response);
+      },
+      () => {
+          console.log("The DELETE observable is now completed.");
+      });
+  }
+
+  editFlight(key: string, flight) {
+    const headers = new HttpHeaders()
+    .set("Content-Type", "application/json");
+ 
+    return this.http.put(this.API_URL+"/flights/"+key+".json",
+    {
+      "origin": flight.origin,
+      "destination": flight.destination,
+      "departureTime": flight.departureTime,
+      "returnTime": flight.returnTime,
+      "code": flight.code,
+      "additionalInformation": flight.additionalInformation,
+      "crew": flight.crew
+    },
+    {headers})
+    .subscribe(
+      (val) => {
+        console.log("POST call successful value returned in body", val);
+      },
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {
+        console.log("The POST observable is now completed.");
+      });
   }
 }
